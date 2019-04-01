4e9c00c13409d230a420af4b9e9c252fab1fa0cc
Waiel Eid <waiel@waieleid.com>
2019-04-01 12:05:14 +0300

    + Created Top toolbar and image view + Added open image picker

--- Meme v1/ViewController.swift
+++ Meme v1/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func openCamera(_ sender: Any) {
+    }
+    
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+    }
 }
 
