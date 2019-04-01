b293a8a6ac6ac7f1c6a8e300822bc7cb99bea848
Waiel Eid <waiel@waieleid.com>
2019-04-01 20:45:36 +0300

    + Test Fields configuration + Keyboard show and hide observers

--- Meme v1/ViewController.swift
+++ Meme v1/ViewController.swift
@@ -8,23 +8,49 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: 2.0
+    ]
+    
+    
+    
     @IBOutlet weak var imagePickerView: UIImageView!
-    
-    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
-    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        // Do any additional setup after loading the view.
+        topTextField.text = "TOP"
+        topTextField.textAlignment = .center
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.textAlignment = .center
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes  = memeTextAttributes
+        subscribeToKeyboardNotifications()
     }
 
 
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+    unsubscribeFromKeyboardNotifications()
+    }
+    
     @IBAction func openCamera(_ sender: Any) {
         
         let imagePicker = UIImagePickerController()
@@ -56,6 +82,45 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
                 let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
                 present(controller,animated: true,completion: nil)
     }
+
+    
+    // When showing lifts screen up
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if bottomTextField.isEditing {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    // When hiding return to original state
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications(){
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    func unsubscribeFromKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    //hide keyboard when return pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        bottomTextField.resignFirstResponder()
+        topTextField.resignFirstResponder()
+        
+        return true
+    }
     
 }
-
