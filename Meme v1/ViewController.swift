0c4c29cd94926e5feb5ab11bbf5f920468c89ad7
Waiel Eid <waiel@waieleid.com>
2019-04-01 18:39:47 +0300

    +  Album and camera image picker + info.plist description for photo library and camera +

--- Meme v1/ViewController.swift
+++ Meme v1/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
+    cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,36 @@ class ViewController: UIViewController {
 
 
     @IBAction func openCamera(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func pickImage(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        present(pickerController, animated: true, completion: nil)
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    
     }
+    
+    // handel image selection from album
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        //close the image picker 
+         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openShare(_ sender: Any) {
+                let image = UIImage()
+                let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+                present(controller,animated: true,completion: nil)
+    }
+    
 }
 
